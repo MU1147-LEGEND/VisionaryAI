688d06e5a011ad3315ed167bdc279964eaf6c1d2
Mohammad Ullah <ullahmohammad314@gmail.com>
2025-06-22 23:59:41 +0600

    Fix formatting in Footer component for improved readability

--- src/components/Shared/Footer.jsx
+++ src/components/Shared/Footer.jsx
@@ -1,10 +1,10 @@
 const Footer = () => {
     return (
         <footer className="w-full bg-inherit text-white m-auto text-center">
-            Created By{" "}
+            Created By 
             <a className="border-b-2 border-blue-500 hover:bg-blue-500 transition-all p-1 rounded-md duration-300" target="_blank" href={"https://facebook.com/fb.mohammadullah"}>
                 Mohammad Ullah
-            </a>{" "}
+            </a> 
             &copy; VisionaryAI
         </footer>
     );
